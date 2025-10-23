c15209f47e8478fbc80e0f65a1675ec794982f17
Stanislaw312 <118776475+Stanislaw312@users.noreply.github.com>
2025-10-24 00:40:36 +0200

    Refactor Toast component to use default import

--- FRONTEND/src/components/Toast/Toast.jsx
+++ FRONTEND/src/components/Toast/Toast.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { toastVersions } from './ToastVersions';
+import ToastVersions from './ToastVersions';
 
 const Toast = ({ message, variant = 'info', duration = 5000, version = 1, onClose }) => {
-  const ToastVersion = toastVersions[version] || toastVersions[1];
+  const ToastVersion = ToastVersions[version] || ToastVersions[1];
   return <ToastVersion message={message} variant={variant} duration={duration} onClose={onClose} />;
 };
 
